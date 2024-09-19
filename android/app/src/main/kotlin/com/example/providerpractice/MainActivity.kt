1966759165bcc6ef8f65159ae364f479c339684b
adhi <108246061+adhi-create@users.noreply.github.com>
2024-09-18 20:17:27 -0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/providerpractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.playMart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/providerpractice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.providerpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
